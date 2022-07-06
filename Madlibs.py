ab50ce45ad04fde6765e824a8f4fec13cf1db0d7
badhon <108542798+badhon496@users.noreply.github.com>
2022-07-06 09:18:00 +0600

    Create Madlibs.py

---
+++ Madlibs.py
@@ -0,0 +1,10 @@
+print("Welcome to madlibs")
+name1 = input("Please input your name: ")
+likeable_things = input("a likeable thing: ")
+expression = input("Input a expression: ")
+mood = input("How is your mood today? ")
+
+
+print(f'Hello {name1}, i hope you are {mood}. i know you like {likeable_things} but in your mind you want to {expression} it.')
+
+# you can add many input and extra line you want.
